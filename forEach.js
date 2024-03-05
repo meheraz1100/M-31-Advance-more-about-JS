d5b035076ca66d98cd6070bbba0910f96f13a993
meheraz1100 <meheraz1100@gmail.com>
2024-03-05 11:15:52 +0600

    use ForEach function in an array and string

--- forEach.js
+++ forEach.js
@@ -1,1 +1,6 @@
-// 31-4 Foreach, Filter, Find, And Differences Between Them
+const numbers = [1, 5 , 6, 4, 15];
+// const result = numbers.forEach(n => n*2);
+const result = numbers.forEach(n => {
+    
+});
+console.log(result);
